733dce7c5ba98336964d25f869ebc4225b73e663
campsluiz <118016772+campsluiz@users.noreply.github.com>
2022-11-11 20:27:28 +0000

    Aula 2 - Aprendemos a dobrar e permitir input

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-# Meu primeiro projeto Pyton 
-# print () = comando de saída
-print ("Hello world!")
+# Comando input: permitir que o usuário digite algo
+nome = input("Digite seu nome: ")
 
-# Quando quiser guardar uma string
-nome = "Camila Luiz"
-# Quando quiserem guardar um código
-idade = 24
-
-print (nome)
-
-# Frase 'minha idade é'
-print ("Meu nome é " +nome)
-print("Minha idade é "+str(idade))
-print (f"Minha idade é {idade}")
-print ("Minha idade é {}".format(idade))
+# Comando de saída (exibir na tela)
+print ("Boa noite, seu nome é {}".format(nome))
 print ("\n")
+idade = int(input("Digite sua idade: "))
+print ("{}, você tem {} anos, muito prazer!".format(nome,idade))
 
-# Quando quiser exibir tudo junto
-print("Meu nome é {} e eu tenho {} anos!".format(nome,idade))
+# Caso queira dobrar a idade
+dobro = idade*2
+print ("O dobro da sua idade seria {}".format(dobro))
+
+# Estrutura por condição
+if idade >= 18:
+  print("Você é maior de idade! Já pode beber ou dirigir")
+else:
+  print("Você é muito jovem para beber...")
+  
+print ("\n")
+  
+  # Gênero: Feminino, Masculino, Prefiro não Identificar
+genero = input("Informe o gênero M=Masculino, F=Feminino ou X=Prefiro Não Identificar: ")
+print ("Tudo bem!, seu nome é {}, você tem {} anos e seu genero é {}".format(nome, idade, genero))
+
+if idade >= 18 and genero == "M":
+  print("... e você também precisa/precisou prestar o serviço militar obrigatório")
+
+
+
