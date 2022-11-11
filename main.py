937810160ecf3c9260133c6bd24537c66d1c06d6
campsluiz <118016772+campsluiz@users.noreply.github.com>
2022-11-11 20:42:03 +0000

    Aula 1 & 2 | Completas

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+# Meu primeiro projeto Pyton 
+# print () = comando de saída
+print ("Hello world!")
+
+# Quando quiser guardar uma string
+nome = "Camila Luiz"
+# Quando quiserem guardar um código
+idade = 24
+
+print (nome)
+
+# Frase 'minha idade é'
+print ("Meu nome é " +nome)
+print("Minha idade é "+str(idade))
+print (f"Minha idade é {idade}")
+print ("Minha idade é {}".format(idade))
+print ("\n")
+
+# Quando quiser exibir tudo junto
+print("Meu nome é {} e eu tenho {} anos!".format(nome,idade))
+
+print ("\n")
 # Comando input: permitir que o usuário digite algo
 nome = input("Digite seu nome: ")
 
